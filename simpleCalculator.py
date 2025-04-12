c1a92d119646f68d795bea1a99e26e8b13987b21
Syed-Mudassir-Ali <hafizmudassir946@gmail.com>
2025-04-12 03:35:56 -0700

    remove commit

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -1,35 +1,3 @@
-# def calculator():
-#     print("Welcome to Simple Calculator!")
-
-#     # Pehla number lo
-#     num1 = float(input("Enter first number: "))
-
-#     # Operator lo (+, -, *, /)
-#     operator = input("Enter operator (+, -, *, /): ")
-
-#     # Dusra number lo
-#     num2 = float(input("Enter second number: "))
-
-#     # Ab check karo operator kya hai
-#     if operator == '+':
-#         result = num1 + num2
-#     elif operator == '-':
-#         result = num1 - num2
-#     elif operator == '*':
-#         result = num1 * num2
-#     elif operator == '/':
-#         if num2 == 0:
-#             result = "Error: Cannot divide by 0"
-#         else:
-#             result = num1 / num2
-#     else:
-#         result = "Invalid operator"
-
-#     print("Result:", result)
-
-# # Function ko call karo
-# calculator()
-
 import streamlit as st
 
 st.title("Simple Calculator")
